a859608593d1a7779043197ad1a786d8db2baa37
Rashaun Skipwith <rashaun.t.skipwith@gmail.com>
2019-02-01 08:38:46 -0600

    Add project files.

---
+++ OpenHouseMarkiV/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace OpenHouseMarkiV
+{
+    class Program
+    {
+        public static string GetUserResponse(string question)
+        {
+            Console.WriteLine(question);
+            string response = Console.ReadLine();
+            return response;
+        }
+
+        public static bool IsAffirmative(string userResponse)
+
+        {
+            userResponse = userResponse.ToUpper();
+
+            if (userResponse == "YEAH" || userResponse == "YES" || userResponse == "Yes" || userResponse == "Please")
+
+            {
+                return true;
+            }
+
+            else
+
+            {
+                return false;
+            }
+
+        }
+
+        public static void ShowRoom(Room roomToShow)
+
+        {
+            string answer = GetUserResponse("Would you like to see the " + roomToShow.Name);
+            if (IsAffirmative(answer))
+
+            roomToShow.Name = "bathroom";
+            roomToShow.Name = "livingroom";
+
+        }
+
+        static void Main(string[] args)
+
+        {
+            Room livingRoom = new Room("Living Room", false, 4, false);
+            livingRoom.Name = "Living Room";
+            livingRoom.HasHardwoods = true;
+            livingRoom.Descriptions.Add("I can see myself living here!");
+
+            Room bathRoom = new Room("bathRoom", true, 2, true);
+            bathRoom.Name = "Bathroom";
+            bathRoom.HasHardwoods = false;
+            bathRoom.Descriptions.Add("bathroom");
+
+            Console.WriteLine("Welcome! Thank you for coming to open house today!");
+
+            string name = GetUserResponse("What is your name?");
+
+            Console.WriteLine("Do you know anyone looking to buy a home in this area?");
+            string answer = Console.ReadLine();
+
+            Console.WriteLine("Do you have time for me to show you the house?");
+            string answer1 = Console.ReadLine();
+
+            Console.WriteLine("How soon will you be interested to mmove to the area?");
+            string answer2 = Console.ReadLine();
+
+            Console.WriteLine("Would you feel confident referring my services to other buyers and sellers?");
+            string answer3 = Console.ReadLine();
+
+            Console.WriteLine("Do you have any questions?");
+            string response = Console.ReadLine();
+
+            if (IsAffirmative(answer))
+            {
+                answer = GetUserResponse("Would you like to see the house?");
+                answer = GetUserResponse("Would you like to see the bathroom?");
+            }
+
+            if (IsAffirmative(answer))
+            { 
+                Console.WriteLine("Do you want to see the heated shower");
+            }
+            if (answer == "Yes")
+
+            {
+                Console.WriteLine("How long have been a home owner?");
+                answer = Console.ReadLine();
+            }
+
+            else
+
+                Console.WriteLine("Are you currently looking to buy property in this area!");
+            string response2 = Console.ReadLine();
+
+            Console.WriteLine("Do you currently own a home");
+            string response3 = Console.ReadLine();
+            answer = Console.ReadLine();
+
+            if (answer == "yes")
+
+            { 
+            Console.WriteLine("Are you thinking about selling or buying a new home!");
+            answer = Console.ReadLine();
+            }
+            else
+                   
+            Console.WriteLine("Tell me, what is your plan for finding your next home!");
+                       
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OpenHouseMarkiV/Room.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenHouseMarkiV
+{
+    class Room
+    {
+        public string Name { get; set; }
+        public bool HasHardwoods { get; set; }
+        public List<string> Descriptions { get; set; }
+        public int numberOFRooms { get; set; }
+
+        
+          public Room(string name, bool hasHardwoods, int numberOFRooms, bool hasGhosts)
+
+          {
+            Name = name;
+            HasHardwoods = hasHardwoods;
+            numberOFRooms = 0;
+            
+          }
+
+
+        static void ShowRoom(Room roomToShow)
+        {
+            string answer = Program.GetUserResponse("Would you like to see" + "bathroom" + roomToShow.Name);
+            if (Program.IsAffirmative(answer))
+
+                Console.WriteLine("Do you want to see the shower");
+
+            if (answer == "Yes")
+            Console.WriteLine("How long have been a home owner?");
+                              
+            else
+
+            {
+                Console.WriteLine("Are you currently looking to buy property in this area!");
+                string response2 = Console.ReadLine();
+
+                Console.WriteLine("Do you currently own a home");
+                string response3 = Console.ReadLine();
+                answer = Console.ReadLine();
+
+
+                Console.WriteLine();
+            }
+                       
+        }
+                          
+    }
+    
+}
+
+    
+
+
